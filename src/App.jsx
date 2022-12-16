cb3c4ec880ba73c0eee332a5641b3f7db0afb962
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2022-12-16 10:43:31 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     how are you my friend
+    </div>
+  )
+}
+
+export default App
